f34b40314baf587ca5045100a04fcdcc536e0aa0
kennyvngo <kennyvungo@gmail.com>
2023-04-21 15:26:36 -0700

    finished two_sum

--- practice-for-ch-ruby-first-tdd-long-practice-main/lib/tdd.rb
+++ practice-for-ch-ruby-first-tdd-long-practice-main/lib/tdd.rb
@@ -6,3 +6,20 @@
         end
         new_array
     end
+
+class Array
+
+    def two_sum
+        new_arr = []
+        i = 0
+        while i < self.length - 1
+            j = i + 1
+            while j < self.length
+                new_arr << [i,j] if self[i] + self[j] == 0
+                j+= 1
+            end
+            i += 1
+        end
+        new_arr
+    end
+end
--- practice-for-ch-ruby-first-tdd-long-practice-main/spec/tdd_spec.rb
+++ practice-for-ch-ruby-first-tdd-long-practice-main/spec/tdd_spec.rb
@@ -10,3 +10,16 @@ describe "#my_uniq" do
         expect(my_uniq([1, 2, 1, 3, 3])).to eq([1, 2, 3])  
     end
 end
+
+describe Array do
+    describe "#two_sum" do
+        subject(:array){[-1, 0, 2, -2, 1].two_sum}
+        it "returns a 2d array" do
+            expect(array[0].class).to eq(Array)
+        end
+
+        it "returns indices of sum of pairs that equal 0" do
+        expect(array).to eq([[0, 4], [2, 3]])
+        end
+    end
+end
